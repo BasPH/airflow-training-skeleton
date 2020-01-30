1b9c06e876e1941929f95d952d89e36cd94660c9
Bas Harenslak <basharenslak@gmail.com>
2020-01-30 22:23:32 +0100

    exercise 1

---
+++ dags/exercise1.py
@@ -0,0 +1,12 @@
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+dag = DAG(dag_id="example_bash_operator", default_args={"owner": "Schuberg Philis"}, schedule_interval=None)
+
+task1 = DummyOperator(task_id="task1")
+task2 = DummyOperator(task_id="task2")
+task3 = DummyOperator(task_id="task3")
+task4 = DummyOperator(task_id="task4")
+task5 = DummyOperator(task_id="task5")
+
+task1 >> task2 >> [task3, task4] >> task5
