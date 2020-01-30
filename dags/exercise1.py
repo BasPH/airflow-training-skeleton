0c38869047c600a923e581995f96668a068f3d52
Bas Harenslak <basharenslak@gmail.com>
2020-01-30 22:26:56 +0100

    set dag

--- dags/exercise1.py
+++ dags/exercise1.py
@@ -3,10 +3,10 @@ from airflow.operators.dummy_operator import DummyOperator
 
 dag = DAG(dag_id="exercise1", default_args={"owner": "Schuberg Philis"}, schedule_interval=None)
 
-task1 = DummyOperator(task_id="task1")
-task2 = DummyOperator(task_id="task2")
-task3 = DummyOperator(task_id="task3")
-task4 = DummyOperator(task_id="task4")
-task5 = DummyOperator(task_id="task5")
+task1 = DummyOperator(task_id="task1", dag=dag)
+task2 = DummyOperator(task_id="task2", dag=dag)
+task3 = DummyOperator(task_id="task3", dag=dag)
+task4 = DummyOperator(task_id="task4", dag=dag)
+task5 = DummyOperator(task_id="task5", dag=dag)
 
 task1 >> task2 >> [task3, task4] >> task5
